b6ca0d2777c0e5aa0edced3b96e4ed70cc8aab55
Golpar Jalali <jalaf552@newschool.edu>
2019-02-17 14:45:53 -0500

    media

--- Assignment_4/src/ofApp.cpp
+++ Assignment_4/src/ofApp.cpp
@@ -8,83 +8,110 @@ void ofApp::setup(){
     midY = ofGetWindowHeight() / 2;
 }
 
+bool drawFirst = true;
+bool drawSecond = true;
+
 //--------------------------------------------------------------
 void ofApp::update(){
-
+    int sec = (ofGetFrameNum() / 60) % 60;
+    drawFirst = sec < 2;
+    drawSecond = sec < 4;
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
     ofSetColor(160,82,45);
+    if (drawFirst){
+        ofDrawTriangle(502, 364, midX, midY, 522, 364);
+        ofDrawCircle(midX+50, midY-50, 7);
+        ofDrawCircle(midX-50, midY-50, 7);
+        ofDrawBezier(midX, midY, 490, 420, 470, 384, 475,midY);
+        ofDrawBezier(midX, midY, 534, 420, 554, 384, 549,midY);
+        //ears
+        ofDrawTriangle(370, 330, 470, 300, 390, 260);
+        ofDrawTriangle(645, 330, 544, 300, 624, 260);
+        //wiskers left
+        ofDrawLine(midX, midY-10, midX-200, midY-50);
+        ofDrawLine(midX, midY-10, midX-200, midY-10);
+        //wiskers right
+        ofDrawLine(midX, midY-10, midX+200, midY-50);
+        ofDrawLine(midX, midY-10, midX+200, midY-10);
+    }
     
-    ofDrawTriangle(502, 364, midX, midY, 522, 364);
-    ofDrawCircle(midX+50, midY-50, 7);
-    ofDrawCircle(midX-50, midY-50, 7);
-    ofDrawBezier(midX, midY, 490, 420, 470, 384, 475,midY);
-    ofDrawBezier(midX, midY, 534, 420, 554, 384, 549,midY);
-    ofDrawTriangle(370, 330, 470, 300, 390, 260);
-    ofDrawTriangle(645, 330, 544, 300, 624, 260);
-    //wiskers left
-    ofDrawLine(midX, midY-10, midX-200, midY-50);
-    ofDrawLine(midX, midY-10, midX-200, midY-10);
-    //wiskers right
-    ofDrawLine(midX, midY-10, midX+200, midY-50);
-    ofDrawLine(midX, midY-10, midX+200, midY-10);
-
-
+    if (drawSecond){
+        ofDrawTriangle(502, 364, midX, midY, 522, 364);
+        ofDrawCircle(midX+50, midY-50, 10);
+        ofDrawCircle(midX-50, midY-50, 10);
+        ofDrawBezier(midX, midY, 490, 420, 470, 384, 475,midY);
+        ofDrawBezier(midX, midY, 534, 420, 554, 384, 549,midY);
+        //ears
+        ofDrawTriangle(370, 330, 470, 300, 390, 300);
+        ofDrawTriangle(645, 330, 544, 300, 624, 300);
+        //wiskers left
+        ofDrawLine(midX, midY-10, midX-200, midY-5);
+        ofDrawLine(midX, midY-10, midX-200, midY);
+        //wiskers right
+        ofDrawLine(midX, midY-10, midX+200, midY-5);
+        ofDrawLine(midX, midY-10, midX+200, midY);
+        
+        
+    }
+    
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::keyReleased(int key){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseMoved(int x, int y ){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseDragged(int x, int y, int button){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mousePressed(int x, int y, int button){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseReleased(int x, int y, int button){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseEntered(int x, int y){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseExited(int x, int y){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::windowResized(int w, int h){
-
+    
 }
 
 //--------------------------------------------------------------
 void ofApp::gotMessage(ofMessage msg){
-
+    
 }
 
 //--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
-
+void ofApp::dragEvent(ofDragInfo dragInfo){
+    
 }
