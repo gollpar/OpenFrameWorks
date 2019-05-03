2a8ab464aaea3067bebf9dbffd8efa8217ffc2b4
Golpar Jalali <42790306+gollpar@users.noreply.github.com>
2019-05-03 14:21:54 -0400

    Add files via upload

---
+++ GAME FINAL/src/ofApp.cpp
@@ -0,0 +1,176 @@
+#include "ofApp.h"
+
+int midX, midY, tot;
+
+//--------------------------------------------------------------
+void ofApp::setup()
+{
+    
+    ofSetCircleResolution(500);
+    ofSetFrameRate(60);
+    
+    
+    blackandwhite.load("picture.jpg");
+    img.load("colored.jpg");
+    
+    //font
+    Comfortaa.load("Comfortaa.ttf",12, true,true);
+    
+    
+    midX = ofGetWindowWidth() / 2;
+    midY = ofGetWindowHeight() / 2;
+    tot = 0;
+   
+    fbo.allocate(img.getWidth(), img.getHeight(),GL_RGBA);
+    
+    
+    
+ 
+    //mask1
+    
+//    img.load("picture.jpg");
+//    fbo.allocate(img.getWidth(), img.getHeight(),GL_RGBA);
+//    fbo.begin();
+//        {
+//           ofClear(0,0,0,0);
+//         }
+//    fbo.end();
+    
+    
+    
+    //Muse
+    const int RECEIVE_PORT   = 7000;
+    receiver.setup(RECEIVE_PORT);
+    
+    //BOOLEANS
+    successString = false;
+    startString = true;
+    
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+    
+    
+    //alpha mask
+    fbo.begin();
+    {
+        ofClear(0,0,0,0);
+    }
+    fbo.end();
+    
+    fbo.begin();
+    {
+        ofClear(0,0,0,0);
+        //ofBackground(255,255,255);
+        ofSetColor(255);
+        ofDrawEllipse(img.getWidth()/2,img.getHeight()/2, tot,tot);
+    }
+    fbo.end();
+    
+    img.getTexture().setAlphaMask(fbo.getTexture());
+    
+    
+    
+    //osc
+    ofxOscMessage m;
+    while(receiver.hasWaitingMessages()){
+        receiver.getNextMessage(m);
+        if (m.getAddress() == "Muse-2C07/elements/alpha_relative"){
+            alpha = m.getArgAsFloat(1);
+            alphaTrans = ofMap(alpha, 0,0.4,1,100);
+            if (alphaTrans != alphaStorage[(index - 1 + store)/store]){
+                alphaStorage[index] = alphaTrans;
+                index = (index + 1) % store;
+            }
+            alphaMean =
+            (alphaStorage[0]+alphaStorage[1]+alphaStorage[2]+alphaStorage[3]+alphaStorage[4])*0.2;
+            //set a total var, so the circle will be more obvious
+            if (alphaMean < 20) {
+                tot += alphaMean;
+            }
+            
+            std::cout << alphaMean << "value" << endl;
+        
+        }
+        
+    }
+    
+}
+
+
+//--------------------------------------------------------------
+void ofApp::draw()
+{
+    
+    blackandwhite.draw(0,0, ofGetWidth(),ofGetHeight());
+    img.draw(0,0, ofGetWidth(),ofGetHeight());
+    
+    
+    
+    //text
+    ofPushStyle();
+    ofSetColor(255);
+    if (successString){
+        Comfortaa.drawString("Yayy! go to the next level.",74,ofGetHeight()-500);
+    }
+    if (startString){
+        Comfortaa.drawString("Concentrate",ofGetWidth()/2-30,ofGetHeight()-60);
+    }
+    ofPopStyle();
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
---
+++ GAME FINAL/src/ofApp.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxOsc.h"
+
+#define store 2
+
+
+class ofApp : public ofBaseApp{
+	public:
+		void setup();
+		void update();
+		void draw();
+		
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y);
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void mouseEntered(int x, int y);
+		void mouseExited(int x, int y);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+    
+    //image mask
+    ofImage img;
+    ofImage blackandwhite;
+    ofFbo fbo;
+
+   
+    //muse
+    ofxOscReceiver receiver;
+    float alpha, alphaTrans,alphaMean;
+    float alphaStorage[store];
+    int index = 0;
+
+    
+    ofTrueTypeFont Comfortaa;
+    
+
+    bool successString;
+    bool startString;
+    
+};
