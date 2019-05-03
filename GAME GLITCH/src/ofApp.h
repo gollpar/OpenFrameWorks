89e7f2a220ffac6f61c434c10ccd09446bef9b5d
Golpar Jalali <42790306+gollpar@users.noreply.github.com>
2019-05-03 14:22:35 -0400

    Add files via upload

---
+++ GAME GLITCH/src/ofApp.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxOsc.h"
+
+#define store 2
+
+
+class ofApp : public ofBaseApp{
+	public:
+		void setup();
+		void update();
+		void draw();
+		
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y);
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void mouseEntered(int x, int y);
+		void mouseExited(int x, int y);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+    
+    //image mask
+    ofImage img;
+    ofFbo fbo;
+    ofFbo fbo2;
+   
+    //muse
+    ofxOscReceiver receiver;
+    float alpha, alphaTrans,alphaMean;
+    float alphaStorage[store];
+    int index = 0;
+
+    
+    ofTrueTypeFont Comfortaa;
+    
+//    bool line;
+//    bool color;
+
+    bool successString;
+    bool startString;
+    
+};
